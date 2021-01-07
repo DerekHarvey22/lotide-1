92cbb1e5666c271575eb57c86f62560ad0ba29f8
Jacob Hein <jacobhein1892@gmail.com>
2021-01-07 22:34:34 +0000

    map lotide

---
+++ map.js
@@ -0,0 +1,10 @@
+const words = [3, 4, 5, 6, 7];
+
+const map = function(array, callback){
+  const results = []
+  for (let item of array) {
+    results.push(callback(item))
+  }
+  return results
+}
+const results1 = map(words, word => word*2);
