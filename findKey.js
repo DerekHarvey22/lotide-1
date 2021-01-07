0a85f7cbe7a902e34e59bd9ec40aefd34f406845
Jacob Hein <jacobhein1892@gmail.com>
2021-01-07 23:22:48 +0000

    kind key lotide

---
+++ findKey.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function (object, callback) {
+  let keyHolder = ''
+
+  for(let names in object){
+    if (callback(object[names])){
+      keyHolder = names
+      break;
+    } else {
+      keyHolder = undefined
+    }
+  }
+  return keyHolder;
+};
+
+assertEqual(
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 4 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 4 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), undefined)
