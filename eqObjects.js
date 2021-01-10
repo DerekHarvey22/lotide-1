bfb6eec14faf178a9c599de8e472d927d5bf84d5
Jacob Hein <jacobhein1892@gmail.com>
2021-01-10 22:45:02 +0000

    eqObjects updated

--- eqObjects.js
+++ eqObjects.js
@@ -1,26 +1,68 @@
 const assertEqual = require('./assertEqual');
-const eqArrays = require(',/eqArrays');
+const eqArrays = require('./eqArrays');
 
 
 const eqObjects = function (object1, object2) {
-  let boolean = true; 
-  let key1 = Object.keys(object1);
-  console.log(key1)
-  let key2 = Object.keys(object2);
-
-  if (key1.length !== key2.length) {
-    boolean = false; 
-  }
-
-  for (let key of key1) {
-    if (Array.isArray(object1[key]) && Array.isArray([object2[key]])) {
-      boolean = eqArrays(object1[key], object2[key]);
-      
-    } else if (object1[key] !== object2[key] && object1[key].length !== object2[key].length) {
-      boolean = false
+  boolean = true;
+  let key1 = Object.keys(object1).sort();
+  let key2 = Object.keys(object2).sort();
+  const digger = function (object) {
+    let array = ''
+    for (let el in object) {
+      if (typeof object[el] === 'object') {
+        array += digger(object[el]);
+      } else if (typeof object[el] === 'number') {
+        array += object[el];
+      }
     }
+    return array;
   }
-  return boolean 
+  if (eqArrays(key1, key2)) {
+    if (digger(object1) === digger(object2)) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
 }
 
-module.exports = eqObjects; 
+
+
+assertEqual(eqObjects({
+  a: {
+    z: 1
+  },
+  b: 2
+}, {
+  b: {
+    z: 1
+  },
+  a: 2
+}), true)
+assertEqual(eqObjects({
+  a: {
+    y: 0,
+    z: 1
+  },
+  b: 2
+}, {
+  a: {
+    z: 1
+  },
+  b: 2
+}), false)
+// // module.exports = eqObjects; 
+assertEqual(eqObjects({
+  c: 3,
+  a: {
+    y: 0,
+    z: 1
+  },
+  b: 2
+}, {
+  a: 1,
+  b: 2
+}), false);
+
