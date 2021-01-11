e47a31e1d9678178e29dc6a98e5cda6e9ae3c431
Jacob Hein <jacobhein1892@gmail.com>
2021-01-11 20:11:09 +0000

    eqArrays refactored

--- eqArrays.js
+++ eqArrays.js
@@ -18,8 +18,6 @@ const eqArrays = function(arr1, arr2) {
 
 const assertEqual = require('./assertEqual');
 
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true) 
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false)
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false) 
+
 
 module.exports = eqArrays; 
